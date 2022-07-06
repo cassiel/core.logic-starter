b5afc4288b28424428bed2c72b1a95626d23124d
Nick (Git client) <nick@cassiel.com>
2022-07-06 09:31:58 +0100

    Tweaks.

--- scratch.clj
+++ scratch.clj
@@ -1,18 +1,49 @@
+(+ 1 1)
+
+(+ 1 (* 2 3))
+
+
+(+ 1 2 3 90 4.5 -6 100)
+
+(def x 1000)
+
+(* x x)
+
+(defn f [x] (* x x x))
+
+(f -2)
+
+[1 20 -5 10 100]
+
+(defn list-of-two [x]
+  [x x])
+
+(list-of-two (list-of-two 99))
+
+(list-of-two (range 100))
+
+(range 100)
+
+(cons 99 (cons 1 [2 3]))
+
+(cons 66 nil)
+
+(* 2 2)
+
 (ns user
   (:require [clojure.core.logic :as L :refer [run* ==]]))
 
-(range 10)
-
 ;; ---
 
-(run* [q]
-  (== q true))
+(run* [x]
+  (== x true))
 
 ;; ---
 
 (run* [q]
   (L/membero q [1 2 3])
-  (L/membero q [2 3 4]))
+  (L/membero q [2 3 4])
+  (L/membero q [99 100]))
 
 ;; ---
 
@@ -28,7 +59,8 @@
 (run* [q]
   (L/conde
    [(== q 1)]
-   [(== q 2)]))
+   [(== q 2)]
+   [(L/membero q [90 91])]))
 
 (run* [q]
   (L/conso 1 [2 3] q))
